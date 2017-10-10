b3135222ce95ea3b81549e95ae26b481d273b65e
MrPitonych <pitonych8@gmail.com>
2017-10-10 19:26:27 +0300

    stage 1

---
+++ src/main/java/ru/mail/polis/pitonych/MyFileDAO.java
@@ -0,0 +1,46 @@
+package ru.mail.polis.pitonych;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.NoSuchElementException;
+
+public class MyFileDAO implements MyDAO {
+    @NotNull
+    private final File dir;
+
+    public MyFileDAO(@NotNull final File dir) {
+        this.dir = dir;
+    }
+
+    private File getFile(@NotNull final String key) {
+        return new File(dir, key);
+    }
+
+    @NotNull
+    @Override
+    public byte[] get(@NotNull String key) throws NoSuchElementException, IllegalArgumentException, IOException {
+        final File file = new File(dir, key);
+        final byte[] value = new byte[(int) file.length()];
+        try (InputStream is = new FileInputStream(file)) {
+            if (is.read(value) != value.length) {
+                throw new IOException("can't read file");
+            }
+        }
+        return value;
+    }
+
+    @Override
+    public void upsert(@NotNull final String key, @NotNull final byte[] value) throws IllegalArgumentException, IOException {
+        try (OutputStream os = new FileOutputStream(getFile(key))) {
+            os.write(value);
+        }
+
+    }
+
+    @NotNull
+    @Override
+    public void delete(String key) throws IllegalArgumentException, IOException {
+        getFile(key).delete();
+    }
+}
---
+++ src/main/java/ru/mail/polis/pitonych/MyService.java
@@ -0,0 +1,105 @@
+package ru.mail.polis.pitonych;
+
+import com.sun.net.httpserver.HttpServer;
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.KVService;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.NoSuchElementException;
+
+public class MyService implements KVService {
+    private static final String PREFIX = "id=";
+    @NotNull
+    private final HttpServer server;
+    @NotNull
+    private final MyDAO dao;
+
+
+    public MyService(int port, @NotNull final MyDAO dao) throws IOException {
+
+        this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        this.dao = dao;
+
+        this.server.createContext("/v0/status", http -> {
+                    final String response = "ONLINE";
+                    http.sendResponseHeaders(200, response.length());
+                    http.getResponseBody().write(response.getBytes());
+                    http.close();
+                });
+
+        this.server.createContext("/v0/entity", http -> {
+            final String id;
+            try {
+                id = extractId(http.getRequestURI().getQuery());
+            } catch (Exception e) {
+                http.sendResponseHeaders(400, 0);
+                http.close();
+                return;
+            }
+
+                    switch (http.getRequestMethod()) {
+
+                        case "GET":
+                            try {
+                                final byte[] getValue = dao.get(id);
+                                http.sendResponseHeaders(200, getValue.length);
+                                http.getResponseBody().write(getValue);
+                            } catch (IOException e) {
+                                http.sendResponseHeaders(404, 0);
+                                http.close();
+                            }
+                            break;
+
+                        case "DELETE":
+                            dao.delete(id);
+                            http.sendResponseHeaders(202, 0);
+                            break;
+
+                        case "PUT":
+                            try {
+                                final int contentLength = Integer.valueOf(http.getRequestHeaders().getFirst("Content-length"));
+                                final byte[] putValue = new byte[contentLength];
+                                if (contentLength != 0 && http.getRequestBody().read(putValue) != putValue.length) {
+                                    throw new IOException("can't read");
+                                }
+
+                                dao.upsert(id, putValue);
+                                http.sendResponseHeaders(201, contentLength);
+                                http.getResponseBody().write(putValue);
+                                break;
+
+                            } catch (NoSuchElementException e) {
+                                http.sendResponseHeaders(404, 0);
+                            }
+                        default:
+                            http.sendResponseHeaders(404, 0);
+                    }
+
+                    http.close();
+
+                });
+    }
+
+    @NotNull
+    private static String extractId(@NotNull final String query){
+        if (!query.startsWith(PREFIX)){
+            throw new IllegalArgumentException("support error");
+        }
+        String key = query.substring(PREFIX.length());
+        if (key.isEmpty()) {
+            throw new IllegalArgumentException("empty id!");
+        }
+        return query.substring(PREFIX.length());
+    }
+
+    @Override
+    public void start() {
+        this.server.start();
+    }
+
+    @Override
+    public void stop() {
+        this.server.stop(0);
+    }
+}
